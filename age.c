88b7a383201f7443594c248d3c15b5f79ff78d3f
Stephie-cpu <wahomestephanie@zetech.ac.ke>
2024-09-10 12:06:11 -0700

    Create age.c

---
+++ age.c
@@ -0,0 +1,9 @@
+//a c programming for(variables) 
+#include<stdio.h>
+int main(){
+    int age=45;
+    printf("Age before:%d",age);
+    age=65;
+    printf("\nAge after:%d",age);
+    return 0;
+}
