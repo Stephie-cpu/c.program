dcbfcc25ec163a05e3f06253627cb47297ad5990
Stephie-cpu <wahomestephanie@zetech.ac.ke>
2024-10-15 09:08:06 +0300

    Create .c

---
+++ .c
@@ -0,0 +1,13 @@
+/ Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i;
+int marks[5]={45,56,87,98,100};
+for(i=0;i<5;i++){
+printf("%d\n",marks[i]);
+}
+
+
+    return 0;
+}
